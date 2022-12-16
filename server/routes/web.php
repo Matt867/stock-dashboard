cdb09a7157681c165ee711578b7c0c25e6651a7f
Matt867 <matthewsidaway@outlook.com>
2022-12-16 16:12:52 +0000

    created login and signup routes with comments

--- server/routes/web.php
+++ server/routes/web.php
@@ -28,42 +28,61 @@ Route::get('/getPrice/{ticker}', function ($ticker) {
 Route::post('/signup', function (Request $request) {
 
     try {
+        // get req body data
         $data = $request->json()->all();
 
+        // if password or username not present throw exception
         if (!isset($data['password']) || !isset($data['username'])) {
             throw new Exception("Password or username not sent");
         }
         
+        // set username and password to local vars
         $username = $data['username'];
         $password = $data['password'];
         
-        DB::insert('INSERT INTO users (username, pass) VALUES (?, ?)', $username, $password);
+        // insert into db
+        DB::insert('INSERT INTO users (username, password) VALUES (?, ?)', array($username, $password));
 
+        // return success!
         return "Created user";
-    } catch (Exception $e) {
+    } catch (Exception $e) {s
         abort(400, $e->getMessage());
     }
 });
 
 Route::post('/login', function (Request $request) {
     try {
-
+        // collect input data from request body
         $data = $request->json()->all();
-
+        // guard clause to check if username or password exist in the request body
         if (!isset($data['username']) || !isset($data['password'])) {
             throw new Exception("Password or username not sent");
         }
+        // prepared statement to query db for username password and id of username with specified username
+        $results = DB::select('SELECT id, username, password FROM users WHERE username=?', array($data['username']));
 
-        $results = DB::select('SELECT pass FROM users WHERE username=?', $data['username']);
+        // guard clause to throw exception if user does not exist
+        if (count($results) < 1) {
+            abort(404, "User does not exist");
+        }
 
-        return var_dump($results);
+        // cast the first result (as usernames are unique) to array
+        $result = (array)$results[0];
+
+        // check given password matches db password
+        if ($data['password'] !== $result['password']) {
+            abort(403);
+        }
+
+        /*
+        * set some key vals in session that will allow us to
+        * get user info without having to constantly ask user to login
+        */
+        session(['id' => $result['id'], 'username' => $result['username']]);
+
+        return "Logged in";
 
     } catch (Exception $e) {
         abort(400, $e->getMessage());
     }
-})
-
-Route::get('/', function () {
-    $results = DB::select('select * from users where id = ?', array(1));
-    return $results;
 });
