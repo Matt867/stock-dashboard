13d18fb49366c5f1d7f3e1285cdfa28eebe36fa4
Ronak <ronakc@meta.com>
2022-12-16 14:44:28 +0000

    database script finished, added associations and fixed formatting

--- server/database/dbsync.php
+++ server/database/dbsync.php
@@ -11,7 +11,7 @@ $drop_old_sells_table = $db->query('DROP TABLE IF EXISTS sellorders');
 $drop_old_portfolio_table = $db->query('DROP TABLE IF EXISTS portfolios');
 
 // Create new tables and associations
-$create_users_table = $db->query('CREATE TABLE users(username text, pass text)');
-$create_buys_table = $db->query('CREATE TABLE buyorders(ordertime timestamp, quantity integer, ticker text, userid integer)');
-$create_sells_table = $db->query('CREATE TABLE sellorders(ordertime timestamp, quantity integer, ticker text, userid integer)');
-$create_portfolio_table = $db->query('CREATE TABLE portfiolios(userid integer, ticker text, quantity integer, updated timestamp)');
+$create_users_table = $db->query('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT, pass TEXT)');
+$create_buys_table = $db->query('CREATE TABLE buyorders(id INTEGER PRIMARY KEY, ordertime TIMESTAMP, quantity INTEGER, ticker TEXT, userid INTEGER, FOREIGN KEY (userid) REFERENCES users (id))');
+$create_sells_table = $db->query('CREATE TABLE sellorders(id INTEGER PRIMARY KEY, ordertime TIMESTAMP, quantity INTEGER, ticker TEXT, userid INTEGER, FOREIGN KEY (userid) REFERENCES users (id))');
+$create_portfolio_table = $db->query('CREATE TABLE portfolios(id INTEGER PRIMARY KEY, userid INTEGER, ticker TEXT, quantity INTEGER, updated TIMESTAMP, FOREIGN KEY (userid) REFERENCES users (id))');
