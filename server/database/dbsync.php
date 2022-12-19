0a1a07642043368a4db7e47b3c8cd4944962526d
Ronak <ronakc@meta.com>
2022-12-19 16:14:25 +0000

    more routes, only a couple left

--- server/database/dbsync.php
+++ server/database/dbsync.php
@@ -11,7 +11,7 @@ $drop_old_sells_table = $db->query('DROP TABLE IF EXISTS sellorders');
 $drop_old_portfolio_table = $db->query('DROP TABLE IF EXISTS portfolios');
 
 // Create new tables and associations
-$create_users_table = $db->query('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT, password TEXT)');
+$create_users_table = $db->query('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)');
 $create_buys_table = $db->query('CREATE TABLE buyorders(id INTEGER PRIMARY KEY, ordertime TIMESTAMP, quantity INTEGER, ticker TEXT, userid INTEGER, FOREIGN KEY (userid) REFERENCES users (id))');
 $create_sells_table = $db->query('CREATE TABLE sellorders(id INTEGER PRIMARY KEY, ordertime TIMESTAMP, quantity INTEGER, ticker TEXT, userid INTEGER, FOREIGN KEY (userid) REFERENCES users (id))');
 $create_portfolio_table = $db->query('CREATE TABLE portfolios(id INTEGER PRIMARY KEY, userid INTEGER, ticker TEXT, quantity INTEGER, updated TIMESTAMP, FOREIGN KEY (userid) REFERENCES users (id))');
